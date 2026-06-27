1a6ca9c65730f47dd38cd4aa4cd5671b7d84224e
Tatiana Morales <tatianamorales170504@gmail.com>
2026-06-26 21:08:02 -0500

    Corregir exportación en pedidosCtrl.js

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-import { crearPedido } from '../controladores/pedidosCtrl.js';
+import { crearPedido } from '../controladores/pedidosCtrl.js'; 
 
 const router = Router();
-
-// Definimos la ruta POST para crear el pedido completo
 router.post('/', crearPedido);
 
 export default router;
