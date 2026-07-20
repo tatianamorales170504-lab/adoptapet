b40f165f8f982fbfd7de0ed5e10e1441a45099ad
Tatiana Morales <tatianamorales170504@gmail.com>
2026-07-20 01:02:59 -0500

    Verificar variables de entorno

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-import {config} from 'dotenv'
-config()
+import { config } from 'dotenv';
+config();
 
-export const BD_HOST=process.env.BD_HOST || localhost
-export const BD_DATABASE=process.env.BD_DATABASE || adoptapet_db
-export const DB_USER=process.env.DB_USER || root
-export const DB_PASSWORD=process.env.DB_PASSWORD || ''
-export const DB_PORT=process.env.DB_PORT || 3306
-export const PORT=process.env.PORT || 3001
+export const BD_HOST = process.env.DB_HOST || 'localhost';
+export const BD_DATABASE = process.env.DB_DATABASE || 'adoptapet_db';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_PORT = Number(process.env.DB_PORT) || 3306;
+export const PORT = Number(process.env.PORT) || 3001;
