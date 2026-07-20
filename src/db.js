1debb6c79fd1431b255288129aacc79c6fd0be4f
Tatiana Morales <tatianamorales170504@gmail.com>
2026-07-20 00:42:13 -0500

    Verificar variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,6 +1,12 @@
 import {createPool} from "mysql2/promise";
 import {BD_HOST,DB_USER,DB_PASSWORD,BD_DATABASE,DB_PORT} from './config.js'
-
+console.log({
+  host: BD_HOST,
+  database: BD_DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD ? "OK" : "VACÍA",
+  port: DB_PORT
+});
 export const conmysql=createPool({
     host:BD_HOST,
     database:BD_DATABASE,
