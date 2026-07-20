13f8ec2da09c465bfd5af0dad3911bedc8a3d429
Tatiana Morales <tatianamorales170504@gmail.com>
2026-07-20 00:29:37 -0500

    Commit dos para AdoptaPet

--- src/db.js
+++ src/db.js
@@ -6,5 +6,9 @@ export const conmysql=createPool({
     database:BD_DATABASE,
     user:DB_USER,
     password:DB_PASSWORD,
-    port:DB_PORT
+    port:DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    
 })
